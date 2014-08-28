1954391e448b42feb4a1d224ab3bea17fbeeb6fb
pyps <gh@pyps.de>
2014-08-28 13:44:43 +0200

    modified yakuake install

--- yakuake/install.sh
+++ yakuake/install.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+#/bin/bash
 
+script="`readlink -f $0`"
+scriptpath="${script%/*}/"
+#foo="/folder/something/file.ex" ; echo "${foo##*/}" ; echo "${foo%/*}"
 
+# symlink the config files
+ln -sf $scriptpath"config/konsolerc" ~/.kde/share/config/
+ln -sf $scriptpath"config/yakuakerc" ~/.kde/share/config/
 
+# symlink themes
+ln -sf $scriptpath"apps/konsole" ~/.kde/share/apps/
+ln -sf $scriptpath"apps/yakuake" ~/.kde/share/apps/
 
 exit 0
+
