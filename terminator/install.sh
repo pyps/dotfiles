d9d35b7c81efd2f54b671136898bce7f29fdf106
pyps <gh@pyps.de>
2014-08-28 13:35:13 +0200

    modified terminator install

--- terminator/install.sh
+++ terminator/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+script="`readlink -f $0`"
+scriptpath="${script%/*}/"
+#foo="/folder/something/file.ex" ; echo "${foo##*/}" ; echo "${foo%/*}"
+
+# symlink the config file
+ln -sf $scriptpath"config" ~/.config/terminator/
 
 exit 0
