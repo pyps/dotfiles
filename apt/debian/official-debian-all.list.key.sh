9089410a4d729f8211446b012a6e07a6bcd2a3ab
pyps <gh@pyps.de>
2015-01-02 13:52:28 +0100

    small changes

--- apt/debian/official-debian-all.list.key.sh
+++ apt/debian/official-debian-all.list.key.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "[+] Adding preferences"
+script="`readlink -f $0`"
+repopath="${script%/*}/"
+#foo="/folder/something/file.ex" ; echo "${foo##*/}" ; echo "${foo%/*}"
+
+pref=official-debian-all.pref
+
+echo "[+] Adding ${pref##*/}"
+sudo ln -sf $repopath$pref /etc/apt/preferences.d/$pref
 
 exit 0
