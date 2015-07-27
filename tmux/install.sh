6cf5eb1e0a97ba56851fb424f07d3658ec5304b0
pyps <gh@pyps.de>
2015-07-27 15:58:56 +0200

    tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,6 +4,8 @@ script="`readlink -f $0`"
 scriptpath="${script%/*}/"
 #foo="/folder/something/file.ex" ; echo "${foo##*/}" ; echo "${foo%/*}"
 
+sudo apt-get install tmux
+
 # symlink tmux config
 ln -sf $scriptpath"tmux.conf" ~/.tmux.conf
 
